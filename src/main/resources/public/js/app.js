82b5122d31ae12e919f480bba493ab8c3e58d4c7
Makumasa <berezamark@gmail.com>
2017-02-17 21:52:58 -0800

    Added validation for ship placement, both in terms of not allowing the user to place ships on top of each other, but also preventing the user from placing a ship outside the board limits.

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -14,51 +14,99 @@ function placeShip() {
    console.log($( "#colSelec" ).val());
    console.log($( "#orientationSelec" ).val());
 
-   //var menuId = $( "ul.nav" ).first().attr( "id" );
-   var request = $.ajax({
-     url: "/placeShip/"+$( "#shipSelec" ).val()+"/"+$( "#rowSelec" ).val()+"/"+$( "#colSelec" ).val()+"/"+$( "#orientationSelec" ).val(),
-     method: "post",
-     data: JSON.stringify(gameModel),
-     contentType: "application/json; charset=utf-8",
-     dataType: "json"
-   });
+   var valid = false;
+   var row_start = parseInt($("#rowSelec").val());
+   var col_start = parestInt($("#colSelec").val());
+   var row_end = row_start;
+   var col_end = col_start;
+   if($("#orientationSelec").val().toUpperCase === "HORIZONTAL") {
+     if($("#shipSelec").val().toUpperCase() === "AIRCRAFTCARRIER") {
+       col_end = col_start + 5;
+     }
+     if($("#shipSelec").val().toUpperCase() === "BATTLESHIP") {
+       col_end = col_start + 4;
+     }
+     if($("#shipSelec").val().toUpperCase() === "CRUISER") {
+       col_end = col_start + 3;
+     }
+     if($("#shipSelec").val().toUpperCase() === "DEESTROYER") {
+       col_end = col_start + 2;
+     }
+     if($("#shipSelec").val().toUpperCase() === "SUBMARINE") {
+       col_end = col_start + 2;
+     }
+   }
 
-   request.done(function( currModel ) {
-     displayGameState(currModel);
-     gameModel = currModel;
+   else {
+     if($("#shipSelec").val().toUpperCase() === "AIRCRAFTCARRIER") {
+       row_end = row_start + 5;
+     }
+     if($("#shipSelec").val().toUpperCase() === "BATTLESHIP") {
+       row_end = row_start + 4;
+     }
+     if($("#shipSelec").val().toUpperCase() === "CRUISER") {
+       row_end = row_start + 3;
+     }
+     if($("#shipSelec").val().toUpperCase() === "DEESTROYER") {
+       row_end = row_start + 2;
+     }
+     if($("#shipSelec").val().toUpperCase() === "SUBMARINE") {
+       row_end = row_start + 2;
+     }
+   }
 
-   });
+   if((row_start > gameModel.aircraftCarrier.end.Across || row_end < gameModel.aircraftCarrier.start.Across || col_start > gameModel.aircraftCarrier.end.Down || col_end < gameModel.aircraftCarrier.start.Down) &&
+      (row_start > gameModel.battleship.end.Across || row_end < gameModel.battleship.start.Across || col_start > gameModel.battleship.end.Down || col_end < gameModel.battleship.start.Down) &&
+      (row_start > gameModel.cruiser.end.Across || row_end < gameModel.cruiser.start.Across || col_start > gameModel.cruiser.end.Down || col_end < gameModel.cruiser.start.Down) &&
+      (row_start > gameModel.destroyer.end.Across || row_end < gameModel.destroyer.start.Across || col_start > gameModel.destroyer.end.Down || col_end < gameModel.destroyer.start.Down) &&
+      (row_start > gameModel.submarine.end.Across || row_end < gameModel.submarine.start.Across || col_start > gameModel.submarine.end.Down || col_end < gameModel.submarine.start.Down) &&
+      row_end <= 10 && col_end >= 10) {
+     var request = $.ajax({
+       url: "/placeShip/"+$( "#shipSelec" ).val()+"/"+$( "#rowSelec" ).val()+"/"+$( "#colSelec" ).val()+"/"+$( "#orientationSelec" ).val(),
+       method: "post",
+       data: JSON.stringify(gameModel),
+       contentType: "application/json; charset=utf-8",
+       dataType: "json"
+     });
 
-   request.fail(function( jqXHR, textStatus ) {
-     alert( "Request failed: " + textStatus );
-   });
+     request.done(function( currModel ) {
+       displayGameState(currModel);
+       gameModel = currModel;
+     });
+
+     request.fail(function( jqXHR, textStatus ) {
+       alert( "Request failed: " + textStatus );
+     });
+   }
+
+   else {
+     alert("Error: Invalid Ship Placement.");
+   }
 }
 
 
 
 
 function fire(){
- console.log($( "#colFire" ).val());
-   console.log($( "#rowFire" ).val());
+  console.log($( "#colFire" ).val());
+  console.log($( "#rowFire" ).val());
 //var menuId = $( "ul.nav" ).first().attr( "id" );
-   var request = $.ajax({
-     url: "/fire/"+$( "#colFire" ).val()+"/"+$( "#rowFire" ).val(),
-     method: "post",
-     data: JSON.stringify(gameModel),
-     contentType: "application/json; charset=utf-8",
-     dataType: "json"
-   });
+  var request = $.ajax({
+    url: "/fire/"+$( "#colFire" ).val()+"/"+$( "#rowFire" ).val(),
+    method: "post",
+    data: JSON.stringify(gameModel),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json"
+  });
 
-   request.done(function( currModel ) {
-     displayGameState(currModel);
-     gameModel = currModel;
+  request.done(function( currModel ) {
+    displayGameState(currModel);
+    gameModel = currModel;
+  });
 
-   });
-
-   request.fail(function( jqXHR, textStatus ) {
-     alert( "Request failed: " + textStatus );
-   });
-
+  request.fail(function( jqXHR, textStatus ) {
+    alert( "Request failed: " + textStatus );
+  });
 }
 
 function scan(){
