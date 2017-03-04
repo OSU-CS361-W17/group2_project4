0560df9180722ce36f12919e3495487906dfc6b2
Makumasa <berezamark@gmail.com>
2017-03-03 21:07:40 -0800

    Fixed tests for the Ship class so now all tests pass. Final code coverage is 94%.

--- tests/edu/oregonstate/cs361/battleship/ShipTest.java
+++ tests/edu/oregonstate/cs361/battleship/ShipTest.java
@@ -11,38 +11,38 @@ class ShipTest {
 
     @Test
     public void testPlaceShip() {
-        Ship s = new Ship("AircraftCarrier",5, new Coordinate(5,2),new Coordinate(5,7),true);
+        Ship s = new Ship("AircraftCarrier",5, new Coordinate(5,2),new Coordinate(5,6),false);
         assertEquals(true,s.covers(new Coordinate(5,2)));
     }
 
     @Test
     public void testPlaceShipHorizontal() {
-        Ship s = new Ship("AircraftCarrier",5, new Coordinate(5,2),new Coordinate(10,2),true);
+        Ship s = new Ship("AircraftCarrier",5, new Coordinate(5,2),new Coordinate(9,2),false);
         assertEquals(true,s.covers(new Coordinate(6,2)));
     }
 
     @Test
     public void testPlaceShipHorizontalFalse() {
-        Ship s = new Ship("AircraftCarrier",5, new Coordinate(5,2),new Coordinate(10,2),true);
+        Ship s = new Ship("AircraftCarrier",5, new Coordinate(5,2),new Coordinate(9,2),false);
         assertEquals(false,s.covers(new Coordinate(1,1)));
     }
 
     @Test
     public void testPlaceShipVerticalFalse() {
-        Ship s = new Ship("AircraftCarrier",5,new Coordinate(5,2),new Coordinate(5,7),true);
+        Ship s = new Ship("AircraftCarrier",5,new Coordinate(5,2),new Coordinate(5,6), false);
         assertEquals(false,s.covers(new Coordinate(1,1)));
     }
 
     //BAD INPUT
     @Test
     public void testPlaceShipVerticalNoLocation() {
-        Ship s = new Ship("AircraftCarrier",5,new Coordinate(0,0),new Coordinate(9,9),true);
+        Ship s = new Ship("AircraftCarrier",5,new Coordinate(0,0),new Coordinate(9,9),false);
         assertEquals(false,s.covers(new Coordinate(1,1)));
     }
 
     @Test
     public void testScan() {
-        Ship s = new Ship("AircraftCarrier",5, new Coordinate(5,2),new Coordinate(5,7),true);
+        Ship s = new Ship("AircraftCarrier",5, new Coordinate(5,2),new Coordinate(5,6),false);
         assertEquals(false,s.scan(new Coordinate(1,1)));
         assertEquals(true,s.scan(new Coordinate(4,2)));
         assertEquals(true,s.scan(new Coordinate(5,2)));
